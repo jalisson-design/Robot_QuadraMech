5ce84c279069b51588a98e4b8b11eadc4d618c79
jalisson-design <j.alisson@usp.br>
2025-12-12 18:26:08 -0300

    Update toolbox1.py

--- utils/toolbox1.py
+++ utils/toolbox1.py
@@ -1,60 +1,5 @@
 import numpy as np
 
-def cinematica_direta_manual(q, l1, l2, l3):
-    """
-    Calcula a posição x, y, z do efetuador manualmente.
-    q: vetor com as posições das juntas [theta1, theta2, theta3, d4]
-    """
-    # Desempacota as variáveis das juntas
-    th1, th2, th3, d4_var = q
-    
-    # Parâmetros DH (theta, d, a, alpha) para cada elo
-    # Elo 1: Rotacional
-    PT1 = [th1, l1, 0, np.pi/2]
-    
-    # Elo 2: Rotacional
-    PT2 = [th2, 0, l2, 0]
-    
-    # Elo 3: Rotacional
-    PT3 = [th3, 0, 0, np.pi/2]
-    
-    # Elo 4: Prismática (theta fixo em 0, d variável)
-    # O offset l3 entra aqui no 'd'
-    PT4 = [0, d4_var + l3, 0, np.pi/2] 
-    
-    # Monta as matrizes individuais
-    A1 = matriz_transformacao(*PT1)
-    A2 = matriz_transformacao(*PT2)
-    A3 = matriz_transformacao(*PT3)
-    A4 = matriz_transformacao(*PT4)
-    
-    # Multiplica em ordem: Base -> T1 -> T2 -> T3 -> T4 -> Efetuador
-    T_final = A1 @ A2 @ A3 @ A4
-    
-    # A posição x, y, z está na última coluna da matriz (índices 0, 1, 2 da coluna 3)
-    x = T_final[0, 3]
-    y = T_final[1, 3]
-    z = T_final[2, 3]
-    
-    return np.array([x, y, z]), T_final
-
-def matriz_transformacao(theta, d, a, alpha):
-    """
-    Função auxiliar que cria a matriz 4x4 de um elo DH padrão
-    """
-    ct = np.cos(theta)
-    st = np.sin(theta)
-    ca = np.cos(alpha)
-    sa = np.sin(alpha)
-    
-    return np.array([
-        [ct, -st*ca,  st*sa, a*ct],
-        [st,  ct*ca, -ct*sa, a*st],
-        [ 0,     sa,     ca,    d],
-        [ 0,      0,      0,    1]
-
-    ])
-
 def traj_quintica(q_start, q_end, tempo_total, dt):
     """
     Baseado no exemplo05.py da monitoria.
@@ -85,3 +30,41 @@ def traj_quintica(q_start, q_end, tempo_total, dt):
         qdd_traj[i, :] = (q_end - q_start) * poly_acc
         
     return t, q_traj, qd_traj, qdd_traj
+
+def cinematica_nova(q, l1, l2, l3): #utilizar essa:
+    
+    th1, th2, th3, d4_var = q
+    C1 = np.cos(th1); S1 = np.sin(th1)
+    C2 = np.cos(th2); S2 = np.sin(th2)
+    C3 = np.cos(th3); S3 = np.sin(th3)
+
+    C23 = C2 * C3 - S2 * S3 # cos(th2 + th3)
+    S23 = S2 * C3 + C2 * S3 # sin(th2 + th3)
+    D4 = l3 + d4_var
+
+    T_final = np.array([
+        [ C1*C23,S1*C23,-S23, 0 ],
+        [ C1*S23,   S1*S23, C23, 0 ],
+        [ -S1, C1, 0, 0 ],
+        [ C1 * (l2 * C2 + D4 * S23),   S1 * (l2 * C2 + D4 * S23),   l1 + l2 * S2 - D4 * C23,   1 ]
+    ]).T 
+
+
+    # T14 = C1*(l2*C2 + D4*S23)+l3*S1
+    # T24 = S1*(l2*C2+D4*S23)-l3*C1
+    # T34 = C23, l1-D4*C23
+
+
+    # T_final = np.array([ 
+    #     [C1*C23, S1, C1*S23, T14],
+    #     [S1*C23, -C1, S1*S23, T24],
+    #     [S23, 0 , T34 ],
+    #     [0,0,0,1]
+    # ])
+
+    x = T_final[0, 3]
+    y = T_final[1, 3]
+    z = T_final[2, 3]
+    
+    return np.array([x, y, z]), T_final
+
